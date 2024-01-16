89bedb67a285b969b0f657963957a85588cc7d2f
leonardo.bombassei <leonardo.bombassei@doubleit.com.br>
2024-01-15 23:20:31 -0300

    feat: Creating header

---
+++ src/components/cart-control.tsx
@@ -0,0 +1,32 @@
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { CartIcon } from "./cart-icon";
+import styled from "styled-components";
+
+const CartCount = styled.span`
+    width: 17px;
+    height: 17px;
+    background-color: var(--delete-color);
+    border-radius: 100%;
+    padding: 0px 5px;
+    font-size: 12px;
+    color: white;
+    margin-left: -10px
+`
+const Container = styled.div`
+    position:relative;
+
+`
+
+export function CartControl() {
+    const { value } = useLocalStorage('cart-items')
+
+
+
+
+    return (
+        <Container>
+            <CartIcon />
+            {value?.length && <CartCount>{value.length}</CartCount>}
+        </Container>
+    )
+}
